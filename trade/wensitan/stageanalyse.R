07752ac5a91e350de08be5a35c1921d55fb7389d
unknown <zhurui1351@gmail.com>
2015-07-17 16:31:55 +0800

    update wensitan

--- trade/wensitan/stageanalyse.R
+++ trade/wensitan/stageanalyse.R
@@ -5,7 +5,7 @@ require('dygraphs')
 path = "D:/data/dest"
 #Sys.setenv(TZ="UTC")
 files = dir(path)
-files=c('SH600004.TXT')
+files=c('SH000001.TXT')
 f = files[1]
 fname = file.path(path,f)
 pricedata = read.zoo(fname,header=FALSE, format = "%m/%d/%Y",sep="\t",index.column=1) 
@@ -50,11 +50,11 @@ y = 1:10
 x1 = seq(from=500,to=600,length.out=10)
 
 #使用累积增长率 滑动窗口
-deltratioSlideLong = na.omit(runSum(deltratio,n=20))
+deltratioSlideLong = na.omit(runSum(deltratio,n=10))
 deltratioSlideShort = na.omit(runSum(deltratio,n=3))
 deltratioSlide = na.omit(merge(deltratioSlideLong,deltratioSlideShort))
 
-m = na.omit(rollapply(deltratioSlide,width=20,FUN=getratio))
+m = na.omit(rollapply(deltratioSlide,width=10,FUN=getratio))
 old.par = par(mfrow=c(2,2))
 
 plot(pweekdata)
@@ -64,12 +64,32 @@ plot(na.omit(deltratioSlideShort-deltratioSlideLong))
 par(old.par)
 
 alldata =merge(pweekdata,deltratioSlide)
-dygraph(pweekdata['2008-03-28/2008-11-28'])
+dygraph(pweekdata)
 
 ns = which(index(pweekdata)=='2013-01-04')
 ne = ns - 25
-
 pweekdata[ns:ne,]
 deltratio[ns:ne,]
 deltratioSlideLong[ns:ne,]
 #猜想 long在 5 - -5 short 2 - -2之间可能是阶段划分的边界
+
+m_alldata = as.matrix(alldata)
+
+stage=apply(m_alldata,MARGIN=1,FUN=function(x){
+  if(is.na(x['deltratioSlideLong']) || is.na(x['deltratioSlideShort']))
+    return(-1)
+  else
+  {
+    if(x['deltratioSlideLong']<= -2 && x['deltratioSlideShort'] <= -2) { return(4) }
+    else if(x['deltratioSlideLong']>= 2 && x['deltratioSlideShort'] >= 2) {return(2)} 
+    else {return(0)}
+  }
+  
+})
+
+names(stage) = ''
+stage = xts(stage,index(alldata))
+alldata = merge(alldata,stage)
+
+#反过来 肉眼判断阶段，利用数据挖掘找到系数 
+#随机抽取m个比如100 形成大的dataframe 再来学习
