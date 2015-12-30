8a7057d6b1e6c2f6b92b031ca7ae79a8c50782cb
unknown <zhurui1351@gmail.com>
2015-12-30 21:21:41 +0800

    a little change

--- trade/bullandbear/analysis.R
+++ trade/bullandbear/analysis.R
@@ -23,19 +23,9 @@ shindex$sma = SMA(Cl(shindex),150)
 
 
 special = list(c(from = '1995-05-18',to='1995-05-22'))
-#上涨100%，跌幅 30%
-bear = list(c(from = '1991-01-02',to='1992-05-26'),c(from = '1992-11-17',to='1993-02-16'),c(from = '1994-07-29',to='1994-09-13')
-           ,c(from = '1996-01-19',to='1997-05-12'),c(from = '1999-05-19',to='2001-06-14')
-            ,c(from = '2005-06-06',to='2007-10-16'),c(from = '2008-10-28',to='2009-08-04'),c(from = '2014-07-22',to='2015-06-12'))
-
-bull = list(c(from = '1992-05-26',to='1992-11-17'),c(from = '1993-02-16',to='1994-07-29'),c(from = '1994-09-13',to='1996-01-19')
-            ,c(from = '1997-05-12',to='1999-05-18'),c(from = '2001-06-14',to='2005-06-06'),c(from = '2007-10-16',to='2008-10-28')
-            ,c(from = '2009-08-04',to='2010-07-02'))
 
 sapply(bear, function(x){as.numeric(as.Date(x['to']) - as.Date(x['from'])) })
 sapply(bull, function(x){as.numeric(as.Date(x['to']) - as.Date(x['from'])) })
-
-
 
 bearlist  = find_bull(upratio = 1,downratio = -0.3,shindex = shindex)
 bearlist = bearlist[1:(length(bearlist)-1)]
@@ -43,7 +33,7 @@ from = bearlist[[3]]['from']
 to = bearlist[[3]]['to']
 end = bearlist[[3]]['end']
 
-beardata = cbind(Cl(shindex[paste(from,end,sep='/')]),shindex[paste(from,end,sep='/')]$sma100)
+beardata = cbind(Cl(shindex[paste(from,end,sep='/')]),shindex[paste(from,end,sep='/')]$sma)
 
 dygraph(beardata)
 
