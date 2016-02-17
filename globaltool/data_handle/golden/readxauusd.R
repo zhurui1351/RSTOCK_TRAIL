3ee65e27b915f052d6e39fbe87c5f32db3c4832f
unknown <zhurui1351@gmail.com>
2016-02-17 08:19:10 +0800

    small change

--- globaltool/data_handle/golden/readxauusd.R
+++ globaltool/data_handle/golden/readxauusd.R
@@ -2,6 +2,6 @@ readxauusd = function(period='days')
 {
   require('quantmod')
   path = "D:/data/黄金/xauusd.csv"
-  pricedata = read.zoo(path,header=T, format = "%Y/%m/%d",sep=",",index.column=1) 
+  pricedata = read.zoo(path,header=T, format = "%Y-%m-%d",sep=",",index.column=1) 
   return(to.period(pricedata,period=period))
 }
--- globaltool/data_handle/japan/macrodata/readmacrodata_jp.R
+++ globaltool/data_handle/japan/macrodata/readmacrodata_jp.R
@@ -4,29 +4,10 @@ readmacrodata_jp = function(path = 'D:/data/各国数据/japan/macro/')
 {
   e = parent.env(environment())
   tableforread = data.frame()
-  r = data.frame(filename = '1年整存整取利率.csv',var = 'interestrate_1y_f_d',fun='interestrate_1y_f_d_fun')
+  r = data.frame(filename = 'CPI.csv',var = 'cpi_m2m_m,cpi_mRm_m,cpi_y2y_y',fun='cpi_fun')
   tableforread = rbind(tableforread,r )
   r = data.frame(filename = 'CPI当月同比.csv',var = 'cpi_m2m_m',fun='cpi_m2m_m_fun' )
   tableforread = rbind(tableforread,r )
-  r = data.frame(filename = 'CPI环比.csv',var = 'cpi_mRm_m',fun='cpi_mRm_m_fun' )
-  tableforread = rbind(tableforread,r)
-  r = data.frame(filename = 'GDP不变价当季同比.csv',var = 'gdp_q2q_q',fun='gdp_q2q_q_fun' )
-  tableforread = rbind(tableforread,r )
-  r = data.frame(filename = 'GDP不变价累计同比.csv',var = 'gdp_sum_q2q_q',fun='gdp_sum_q2q_q_fun' )
-  tableforread = rbind(tableforread,r )
-  r = data.frame(filename = 'M2.csv',var = 'm2_m',fun='m2_m_fun' )
-  tableforread = rbind(tableforread,r )
-  r = data.frame(filename = 'M2同比.csv',var = 'm2_m2m_m',fun='m2_m2m_m_fun' )
-  tableforread = rbind(tableforread,r )
-  r = data.frame(filename = 'PPI工业品当月同比.csv',var = 'ppi_m2m_m',fun='ppi_m2m_m_fun' )
-  tableforread = rbind(tableforread,r )
-  r = data.frame(filename = '就业人数合计同比增减.csv',var = 'emp_m2m_q',fun='emp_m2m_q_fun' )
-  tableforread = rbind(tableforread,r )
-  r = data.frame(filename = '平均汇率美元兑人民币.csv',var = 'exchg_usd_rmb_m',fun='exchg_usd_rmb_m_fun' )
-  tableforread = rbind(tableforread,r )
-  r = data.frame(filename = '消费者信心指数.csv',var = 'cci_m',fun='cci_m_fun' )
-  tableforread = rbind(tableforread,r )
-  
   for(i in 1:nrow(tableforread))
   {
     r = tableforread[i,]
@@ -34,9 +15,9 @@ readmacrodata_jp = function(path = 'D:/data/各国数据/japan/macro/')
     tb = read.csv(f)
     fun = as.character(r[,'fun'])
     var = as.character(r[,'var'])
+    var = unlist(strsplit(var,','))
     if(fun =='')
     {
-      #assign(var,tb)
       assign(var,tb,envir=e)
     }
     else
@@ -44,7 +25,10 @@ readmacrodata_jp = function(path = 'D:/data/各国数据/japan/macro/')
       tmpcall = call(fun,tb)
       tmp = eval(tmpcall)
       #assign(var,tmp)
-      assign(var,tmp,envir=e)
+      for(j in 1 : length(var))
+      {
+        assign(var[j],tmp[[j]],envir=e)
+      }
     }
   }
   return(tableforread)
