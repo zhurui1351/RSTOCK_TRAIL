90eefab5a169f044e145277e64a6d6a0ee4ed704
unknown <zhurui1@CDJR-FD09111754.360buyAD.local>
2016-02-18 10:37:08 +0800

    add soybean stats

--- globaltool/data_handle/commodity/soybean.R
+++ globaltool/data_handle/commodity/soybean.R
@@ -1,14 +1,24 @@
-read_dou1_ch = function()
+require(quantmod)
+read_dou1_ch = function(path = 'D:/data/commidity/soybean/d/')
 {
-  
+  fname = paste(path,'dou1day.txt',sep='')
+  pricedata = read.zoo(fname,header=FALSE, format = "%m/%d/%Y",sep=",",index.column=1) 
+  colnames(pricedata)<-c("Open","High","Low","Close","Volume","Amount","settle")
+  return(pricedata)
 }
 
-read_dou2_ch = function()
+read_dou2_ch = function(path = 'D:/data/commidity/soybean/d')
 {
-  
+  fname = paste(path,'dou2day.txt',sep='')
+  pricedata = read.zoo(fname,header=FALSE, format = "%m/%d/%Y",sep=",",index.column=1) 
+  colnames(pricedata)<-c("Open","High","Low","Close","Volume","Amount","settle")
+  return(pricedata)
 }
 
-read_s_f_us = function()
+read_s_f_us = function(path = 'D:/data/commidity/soybean/d')
 {
-  
+  fname = paste(path,'s_f_d.csv',sep='')
+  pricedata = read.csv(fname,header=T,sep=",") 
+  pricedata = xts(pricedata[,2:7],order.by = as.Date(pricedata$Date))
+  return(pricedata)
 }
