f14719415e8487422c1d9f0364aedb0a14d3b9e2
unknown <zhurui1@CDJR-FD09111754.360buyAD.local>
2016-03-10 21:35:24 +0800

    event

--- trade/commodity/soybean/eventStatic.R
+++ trade/commodity/soybean/eventStatic.R
@@ -6,48 +6,18 @@ dou1 = read_dou1_d_wind()
 dou1$votile = dou1$Close - dou1$Open
 dou1$maxvotile = dou1$High - dou1$Low
 
+#A股冲击
 shindex = read_sh000001()
 shindex$votitleratio = Delt(shindex$Close)#(shindex$Close - shindex$Open) / shindex$Open
-dates = index(shindex)[which(shindex$votitleratio  <  -0.055)]
-
-id = c()
-for(d in dates)
-{
-  i = findInterval(as.Date(d),index(dou1))
-  #i = which(index(dou1) == d)
-  if( i == 0) next
-  id = c(id,i)
-}
-
-result = data.frame()
-for(i in id)
-{
-  sep  = 10
-  pre = (i - 10) : i
-  if(i + 2 > length(index(dou1))) next
-  after = i+2
-  
-  prevotile = dou1[pre,]$votile
-  premean = mean(prevotile)
-  
-  
-  preratio = length(prevotile[prevotile>0])/length(prevotile)
-
-  aftervotile = dou1[after,]$votile
-  
-  
-  result = rbind(result,data.frame(meanchg = premean, preratio = preratio , aftervotile = aftervotile ))
-}
+dates = index(shindex)[which(shindex$votitleratio  <  -0.05)]
+basicEventStatic(dou1[,1:4],dates,30,1)
 
 
-subdou1 = dou1[id+1,]
-nrow(subdou1)
-length(dou1$votile[dou1$votile > 0]) / length(dou1$votile)
-
-length(subdou1$votile[subdou1$votile < 0]) / length(subdou1$votile)
-prop.test(length(subdou1$votile[subdou1$votile < 0]),length(subdou1$votile))
 #汇市冲击
 usdcny_d = readusdcny_d()
 usdcny_d_change = diff(usdcny_d) * 10000
 
 dates =index(usdcny_d_change[usdcny_d_change > 100]) 
+basicEventStatic(dou1[,1:4],dates,30,1:30)
+
+#经济事件 社会政治事件 自然事件 疫情 新奥尔良飓风灾难 hurricanes 季节性 气候
---
+++ trade/eventAnalysis/basicEventStatic.R
@@ -0,0 +1,56 @@
+basicEventStatic = function(pricedata,eventdates,preperd,afterperd)
+{
+  pricedata$votile = pricedata$Close - pricedata$Open 
+  
+  id = c()
+  for(d in dates)
+  {
+    i = findInterval(as.Date(d),index(pricedata))
+    #i = which(index(pricedata) == d)
+    if( i == 0) next
+    id = c(id,i)
+  }
+  
+  result = data.frame()
+  for(i in id)
+  {
+    sep  = preperd
+    pre = (i - preperd):i
+      
+    if(pre[1] < 1) next;
+    
+    startafter = i + afterperd[1]
+    endafter =  i + afterperd[length(afterperd)]
+    
+    if(endafter > length(index(pricedata))) next
+
+    prevotile = pricedata[pre,]$votile
+    premean = mean(abs(prevotile),na.rm=T)
+    
+    preupratio = length(prevotile[prevotile>0])/length(prevotile)
+    aftervotile = as.numeric(pricedata[endafter,]$Close) - as.numeric(pricedata[startafter,]$Open)
+    
+    votileafter = pricedata[startafter:endafter,]$votile
+    aftermean = mean(abs(votileafter),na.rm=T)
+    
+    result = rbind(result,data.frame(date=index(pricedata)[i],premean = premean,aftermean=aftermean, preupratio = preupratio,aftervotile=aftervotile ))
+  }
+  
+  totalnum = nrow(result)
+  totolvotile = sum(result$aftervotile)
+  premean = mean(result$premean)
+  aftermean = mean(result$aftermean)
+  preupratio = mean(result$preupratio)
+  afterupratio = result$aftervotile
+  afterupratio = length(afterupratio[afterupratio>0])/length(afterupratio)
+  print('result:')
+  print(result)
+  print(paste('totalnum:',totalnum))
+  print(paste('totolvotile:',totolvotile))
+  print(paste('premean:',premean))
+  print(paste('aftermean:',aftermean))
+  print(paste('preupratio:',preupratio))
+  print(paste('afterupratio:',afterupratio))
+  print('binom test,preupratio != afterupratio')
+  print(binom.test(floor(totalnum*preupratio),totalnum,p=afterupratio))
+}
