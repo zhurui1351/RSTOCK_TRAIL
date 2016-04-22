34cd3f5f4a37158b1432c64d292916baae36cf15
unknown <zhurui1@CDJR-FD09111754.360buyAD.local>
2016-04-22 17:40:53 +0800

    small

--- trade/commodity/soybean/bullandbearstatic.R
+++ trade/commodity/soybean/bullandbearstatic.R
@@ -4,7 +4,7 @@ source('D:/Rcode/code/RSTOCK_TRAIL/globaltool/readdata.R')
 sourceDir('D:/Rcode/code/RSTOCK_TRAIL/trade/eventAnalysis')
 sourceDir('D:/Rcode/code/RSTOCK_TRAIL/trade/bullandbear/help',encoding = 'utf8')
 dou1 = read_dou1_d_wind()
-bulllist  = find_bull(ispoint=T,uppoint = 400,downpoint = -100,shindex = na.omit(dou1[,1:4]))
+bulllist  = find_bull(ispoint=T,uppoint = 600,downpoint = -200,shindex = na.omit(dou1[,1:4]))
 list_l  = bulllist
 myindex = na.omit(dou1[,1:4])
 for(i in 1:(length(list_l )-1))
@@ -21,7 +21,7 @@ for(i in 1:(length(list_l )-1))
 
 }
 
-bearlist = find_bear(uppoint = 100,downpoint = -400,shindex = na.omit(dou1[,1:4]),ispoint = T)
+bearlist = find_bear(uppoint = 200,downpoint = -600,shindex = na.omit(dou1[,1:4]),ispoint = T)
 list_l  = bearlist
 myindex = na.omit(dou1[,1:4])
 for(i in 1:(length(list_l )-1))
@@ -37,3 +37,13 @@ for(i in 1:(length(list_l )-1))
   plotdata = cbind(Cl(myindex[statdate]),myindex[statdate]$sma)
   
 }
+
+
+dou_us_quandl = read_s1_quandl()
+list_l  = bulllist
+for( i in 1:(length(list_l )-1))
+{
+  
+}
+
+
