accbb99d82f7f88493d39ed063de4573ba2cb186
zhurui1351 <zhurui1351@gmail.com>
2016-09-27 18:07:01 +0800

    pydata

--- pyfordata/start_1.py
+++ pyfordata/start_1.py
@@ -231,3 +231,143 @@ arr[[1, 5, 7, 2]][:, [0, 3, 1, 2]]
 arr[np.ix_([1, 5, 7, 2], [0, 3, 1, 2])]
 #转置和对换
 
+arr = np.arange(15).reshape((3, 5))
+arr
+arr.T
+
+arr = np.random.randn(6, 3)
+np.dot(arr.T, arr)
+
+
+arr = np.arange(16).reshape((2, 2, 4))
+arr
+arr.transpose((1, 0, 2))
+arr.swapaxes(1, 2)
+
+arr = np.arange(10)
+np.sqrt(arr)
+np.exp(arr)
+
+x = randn(8)
+y = randn(8)
+x
+y
+np.maximum(x, y)
+
+arr = randn(7) * 5
+np.modf(arr) 
+
+points = np.arange(-5, 5, 0.01) # 1000 equally spaced points
+xs, ys = np.meshgrid(points, points)
+ys
+from matplotlib.pyplot import imshow, title
+import matplotlib.pyplot as plt
+z = np.sqrt(xs ** 2 + ys ** 2)
+z
+plt.imshow(z, cmap=plt.cm.gray); plt.colorbar()
+plt.title("Image plot of $\sqrt{x^2 + y^2}$ for a grid of values")
+plt.draw()
+xarr = np.array([1.1, 1.2, 1.3, 1.4, 1.5])
+yarr = np.array([2.1, 2.2, 2.3, 2.4, 2.5])
+cond = np.array([True, False, True, True, False])
+result = [(x if c else y)
+          for x, y, c in zip(xarr, yarr, cond)]
+result
+
+result = np.where(cond, xarr, yarr)
+result
+
+
+arr = randn(4, 4)
+arr
+np.where(arr > 0, 2, -2)
+np.where(arr > 0, 2, arr) # set only positive values to 2
+
+
+arr = np.random.randn(5, 4) # normally-distributed data
+arr.mean()
+np.mean(arr)
+arr.sum()
+arr.mean(axis=1)
+arr.sum(0)
+
+arr = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+arr.cumsum(0)
+arr.cumprod(1)
+
+arr = randn(100)
+(arr > 0).sum() # Number of positive values
+
+bools = np.array([False, False, True, False])
+bools.any()
+bools.all()
+#排序
+arr = randn(8)
+arr
+arr.sort()
+arr
+
+arr = randn(5, 3)
+arr
+arr.sort(1)
+arr
+
+large_arr = randn(1000)
+large_arr.sort()
+large_arr[int(0.05 * len(large_arr))] # 5% quantile
+
+
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+np.unique(names)
+ints = np.array([3, 3, 3, 2, 2, 1, 1, 4, 4])
+np.unique(ints)
+
+values = np.array([6, 0, 0, 3, 2, 5, 6])
+np.in1d(values, [2, 3, 6])
+
+#读写文件
+arr = np.arange(10)
+np.save('some_array', arr)
+np.load('some_array.npy')
+np.savez('array_archive.npz', a=arr, b=arr)
+arch = np.load('array_archive.npz')
+arch['b']
+!rm some_array.npy
+!rm array_archive.npz
+!cat array_ex.txt
+arr = np.loadtxt('array_ex.txt', delimiter=',')
+arr
+#线性代数
+x = np.array([[1., 2., 3.], [4., 5., 6.]])
+y = np.array([[6., 23.], [-1, 7], [8, 9]])
+x
+y
+x.dot(y)  # equivalently np.dot(x, y)
+
+np.dot(x, np.ones(3))
+
+np.random.seed(12345)
+from numpy.linalg import inv, qr
+X = randn(5, 5)
+mat = X.T.dot(X)
+inv(mat)
+mat.dot(inv(mat))
+q, r = qr(mat)
+r
+#随机数生成
+samples = np.random.normal(size=(4, 4))
+samples
+from random import normalvariate
+N = 1000000
+%timeit samples = [normalvariate(0, 1) for _ in xrange(N)]
+%timeit np.random.normal(size=N)
+#随机漫步
+np.random.seed(12345)
+nsteps = 1000
+draws = np.random.randint(0, 2, size=nsteps)
+steps = np.where(draws > 0, 1, -1)
+walk = steps.cumsum()
+walk.min()
+walk.max()
+(np.abs(walk) >= 10).argmax()
+#pandas学习
